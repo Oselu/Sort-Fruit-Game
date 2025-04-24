799a36f7a33ecd08c3e3f53d0c17531454c16594
ExtortionBaby <58045149+Oselu@users.noreply.github.com>
2025-04-24 22:03:14 +0100

    Update index.js code

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
 let appleShelf = document.getElementById("apple-shelf")
 let orangeShelf = document.getElementById("orange-shelf")
 
+function arrangeFruit (){
+for (let arrayindex=0; arrayindex < fruit.length; arrayindex += 1)
+    if (fruit[arrayindex] === "🍎"){
+        appleShelf.textContent += fruit [arrayindex]
+    }
+    else if (fruit[arrayindex] === "🍊"){
+        orangeShelf.textContent += fruit[arrayindex]
+    } 
+}
+
+arrangeFruit()
 // Create a function that puts the apples onto the appleShelf
 // and the oranges onto the orangeShelf. Use a for loop,
 // a conditional statement, and the textContent property.
